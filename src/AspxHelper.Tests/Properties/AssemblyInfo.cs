aae40e88648ce27bf709f8f950c07ce32d95ce68
Eric Newton <ericnewton76@gmail.com>
2018-01-05 19:59:50 -0500

    updated assemblyinfo

--- src/AspxHelper.Tests/Properties/AssemblyInfo.cs
+++ src/AspxHelper.Tests/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cpcontrib.aspxhelper_Tests")]
+[assembly: AssemblyTitle("AspxHelper.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("github.com/cpcontrib")]
 [assembly: AssemblyProduct("nunit.tests")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
--- src/AspxHelper/Properties/AssemblyInfo.cs
+++ src/AspxHelper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cpcontrib.aspxhelper")]
+[assembly: AssemblyTitle("cpcontrib.AspxHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cpcontrib.aspxhelper")]
+[assembly: AssemblyCompany("github.com/cpcontrib")]
+[assembly: AssemblyProduct("cpcontrib.AspxHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
